1f2e3f720605a824c070110bfc41f277e6ef343e
Salma Rosas <a18490977@itmexicali.edu.mx>
2021-12-03 16:01:55 -0800

    actualizacion agregar mas paginas

---
+++ CLAUCRIS/JS/prueba.js
@@ -0,0 +1,27 @@
+//desplegar animacion
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+
+	});
+
+};
+
+//animacion de cuadros
+AOS.init();
